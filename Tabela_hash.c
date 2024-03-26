4bf49e2682a58cd1cffb6df90292e75fb5a0f269
Nigguti <164106348+Nigguti@users.noreply.github.com>
2024-03-26 10:11:46 -0300

    hash

--- Tabela_hash.c
+++ Tabela_hash.c
@@ -31,3 +31,52 @@ int busca(int t[], int chave){
     }
     return 0;
 }
+
+void imprimir(int t[]){
+    int i;
+    for(i=0; i<TAM; i++){
+        printf("%d = %d", i, t[id]);
+    }
+}
+
+int main(void) {
+    int opcao, valor, retorno, tabela[TAM];
+
+    iniciarTabela(tabela);
+
+    do{
+        printf("\n
+        0 - Sair
+        1 - Inserir
+        2 - Buscar
+        3 - Imprimir
+        \n");
+        scanf("%d", &opcao);
+
+        wwitch(opcao){
+
+            case 1:
+            printf("\nValor: ");
+            scanf("%d", &valor);
+            inserir(tabela, valor);
+        break;
+
+            case 2:
+            printf("\nValor: ");
+            scanf("%d", &valor);
+            retorno = busca(tabela, valor);
+            if(retorno != 0);
+            printf("\nValor encontrado: %d\n", retorno);
+            else
+            printf("\nValor não encontrado\n");
+        break;
+
+            case 3:
+            imprimir(tabela);
+            break;
+
+            default:
+            printf("\nOpção inválida.\n");
+        }
+    }while(opcao != 0);
+}
